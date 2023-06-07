190b35c1d4b40563e6e95cf969b0cc0bb906f8b9
CAIO ALVES CARVALHO <caio.5944@aluno.pr.senac.br>
2023-06-07 16:52:16 -0300

    refactor: duas classes foram adicionadas

--- main.py
+++ main.py
@@ -7,58 +7,90 @@ from doctest import master
 customtkinter.set_appearance_mode("light")
 customtkinter.set_default_color_theme("green")
 
-telaLogin = customtkinter.CTk()
-telaLogin.title("Clockin - Login")
-telaLogin.config(bg='#D2DDF9')
-telaLogin.geometry("900x800")
 
-#Functions
+class telaCadastro(customtkinter.CTkToplevel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.geometry("900x800")
+        self.title("Clockin - Cadastro")
+        self.config(bg='#D2DDF9')
+
+        # Declarations
+        font_title = customtkinter.CTkFont(family='', size=20, weight='bold')
+        font_button = customtkinter.CTkFont(family='', size=15, weight='bold')
+        font_link = customtkinter.CTkFont(family='inter', size=13, weight='bold', underline=True)
+        font_creator = customtkinter.CTkFont(family='inter', size=15)
+
+
+class TelaLogin(customtkinter.CTk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.geometry("900x800")
+        self.title("Clockin - Login")
+        self.config(bg='#D2DDF9')
+
+        # Declarations
+        font_title = customtkinter.CTkFont(family='', size=20, weight='bold')
+        font_button = customtkinter.CTkFont(family='', size=15, weight='bold')
+        font_link = customtkinter.CTkFont(family='inter', size=13, weight='bold', underline=True)
+        font_creator = customtkinter.CTkFont(family='inter', size=15)
+
+        logo = customtkinter.CTkImage(light_image=Image.open("images/logo-clockin.png"),
+                                      dark_image=Image.open("images/logo-clockin.png"),
+                                      size=(110, 110))
+
+        text_logo = customtkinter.CTkImage(light_image=Image.open("images/text-clockin.png"),
+                                           dark_image=Image.open("images/text-clockin.png"),
+                                           size=(130, 30))
+
+        # Creation of elements
+        self.image_logo = customtkinter.CTkLabel(self, image=logo, text="", fg_color='#D2DDF9')  # display image with a CTkLabel
+        self.image_logo.place(relx=0.5, rely=0.16, anchor=tkinter.CENTER)
+
+        self.image_text_logo = customtkinter.CTkLabel(self, image=text_logo, text="", fg_color='#D2DDF9')  # display image with a CTkLabel
+        self.image_text_logo.place(relx=0.5, rely=0.27, anchor=tkinter.CENTER)
+
+        frame = customtkinter.CTkFrame(master=master, width=400, height=255, corner_radius=15, fg_color='#F2F2F2', bg_color='#D2DDF9')
+        frame.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+
+        title = customtkinter.CTkLabel(master=frame, text='Logar:', text_color='#7D7987', font=font_title)
+        title.place(x=35, y=30)
+
+        login = customtkinter.CTkEntry(master=frame, width=330, height=40, placeholder_text="Seu login")
+        login.place(x=35, y=70)
+
+        password = customtkinter.CTkEntry(master=frame, width=330, height=40, placeholder_text="Sua senha", show="*")
+        password.place(x=35, y=130)
+
+        login_button = customtkinter.CTkButton(master=frame, width=100, height=40, text='LOGIN', corner_radius=6, font=font_button, fg_color='#FAA115', hover_color='#FBA827',
+                                               command=clique)
+        login_button.place(x=150, y=190)
+
+        self.cadastro_button = customtkinter.CTkButton(self, text='CADASTRE-SE', text_color='#3F5B80', fg_color='transparent', bg_color='#D2DDF9', hover=False,
+                                                       font=font_link, command=self.open_telaCadastro)
+        self.cadastro_button.place(relx=0.5, rely=0.7, anchor=tkinter.CENTER)
+
+        self.toplevel_window = None
+
+        self.creator = customtkinter.CTkLabel(self, text='Criado por Caio Carvalho', text_color='#7D7987', bg_color='#D2DDF9', font=font_creator)
+        self.creator.place(relx=0.5, rely=0.96, anchor=tkinter.CENTER)
+
+    def open_telaCadastro(self):
+        if self.toplevel_window is None or not self.toplevel_window.winfo.exists():
+            self.toplevel_window = telaCadastro(self)  # create window if its None or destroyed
+            TelaLogin.withdraw(self)
+        else:
+            self.toplevel_window.focus()  # if window exists focus it
+
+
+# Functions
 def clique():
     print("Login realizado")
 
-#Declarations
-font_title = customtkinter.CTkFont(family='', size=20, weight='bold')
-font_button = customtkinter.CTkFont(family='', size=15, weight='bold')
-font_link = customtkinter.CTkFont(family='inter', size=13, weight='bold', underline=True)
-font_creater = customtkinter.CTkFont(family='inter', size=15)
 
-logo = customtkinter.CTkImage(light_image=Image.open("images/logo-clockin.png"),
-                                  dark_image=Image.open("images/logo-clockin.png"),
-                                  size=(110, 110))
-
-text_logo = customtkinter.CTkImage(light_image=Image.open("images/text-clockin.png"),
-                                  dark_image=Image.open("images/text-clockin.png"),
-                                  size=(130, 30))
-
-#Creation of elements
-image_logo = customtkinter.CTkLabel(telaLogin, image=logo, text="", fg_color='#D2DDF9')  # display image with a CTkLabel
-image_logo.place(relx=0.5, rely=0.16, anchor=tkinter.CENTER)
-
-image_text_logo = customtkinter.CTkLabel(telaLogin, image=text_logo, text="", fg_color='#D2DDF9')  # display image with a CTkLabel
-image_text_logo.place(relx=0.5, rely=0.27, anchor=tkinter.CENTER)
-
-frame = customtkinter.CTkFrame(master=master, width=400, height=255, corner_radius=15, fg_color='#F2F2F2', bg_color='#D2DDF9')
-frame.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
-
-title = customtkinter.CTkLabel(master=frame, text='Logar:', text_color='#7D7987', font=font_title)
-title.place(x=35, y=30)
-
-login = customtkinter.CTkEntry(master=frame, width=330, height=40, placeholder_text="Seu login")
-login.place(x=35, y=70)
-
-senha = customtkinter.CTkEntry(master=frame, width=330, height=40, placeholder_text="Sua senha", show="*")
-senha.place(x=35, y=130)
-
-botao_login = customtkinter.CTkButton(master=frame, width=100, height=40, text='LOGIN', corner_radius=6, font=font_button, fg_color='#FAA115', hover_color='#FBA827', command=clique)
-botao_login.place(x=150, y=190)
-
-cadastre_se= customtkinter.CTkLabel(telaLogin, text='CADASTRE-SE', text_color='#3F5B80', bg_color='#D2DDF9', font=font_link)
-cadastre_se.place(relx=0.5, rely=0.7, anchor=tkinter.CENTER)
-
-creater= customtkinter.CTkLabel(telaLogin, text='Criado por Caio Carvalho', text_color='#7D7987', bg_color='#D2DDF9', font=font_creater)
-creater.place(relx=0.5, rely=0.96, anchor=tkinter.CENTER)
-
-telaLogin.mainloop()
+def troca():
+    print("Indo para tela de cadastro...")
 
 
-#border_width=1, border_color='#828282'
+app = TelaLogin()
+app.mainloop()
